7aceb960610c1594631aa614b9a9ed31c804a49f
juan carlos <juancramirez823@gmail.com>
2022-03-28 19:51:56 -0600

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "hola como estas";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 7;
+const nuevoNum = 8;
 
 // Crea una variable booleana:
 const nuevoBool = false;
